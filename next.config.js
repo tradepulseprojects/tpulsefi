ffe9fd8cb0bbd8299dfcaef76994fc0bcb6ddf2b
tradepulseprojects <tradepulsetpl@gmail.com>
2025-04-14 17:41:16 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      // This tells Next.js to ignore ESLint errors during builds
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
